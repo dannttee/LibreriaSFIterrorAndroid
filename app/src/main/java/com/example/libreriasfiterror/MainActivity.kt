b52a9dd55eddb2aa830abfb72d1c38d53e7972fb
PV-Alumno <PV-Alumno@duoc.acad>
2025-09-26 16:39:31 -0300

    Inicio de proyecto estructura base MVVM

---
+++ app/src/main/java/com/example/libreriasfiterror/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.libreriasfiterror
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.libreriasfiterror.ui.theme.LibreriaSfiterrorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LibreriaSfiterrorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LibreriaSfiterrorTheme {
+        Greeting("Android")
+    }
+}
